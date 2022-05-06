24b23cb9b9101223f50c0437d0796f7308adfbbb
gioripa <giorgio.ripamonti@studenti.unimi.it>
2022-05-06 13:06:34 +0200

    api roma

--- js/weather.js
+++ js/weather.js
@@ -1,12 +1,40 @@
-async function getWeather(){
-    let response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=45.4642&lon=9.19&appid=40aa9efaf5c5149435908a060eb5baf9&units=metric",{
-    method:"GET"
-    })
-    //fetch("https://api.openweathermap.org/geo/1.0/direct?q=Milan&appid=40aa9efaf5c5149435908a060eb5baf9",{
-    // method:"GET"
-    // });
-    let jsonObj = await response.json();
-    console.log(jsonObj);
-    document.getElementById('weather-temp').innerText= jsonObj.main.temp + "°";
+
+const API = "40aa9efaf5c5149435908a060eb5baf9";
+
+const request = async url => {
+    const response = await fetch(url);
+    return response.ok ? response.json() : Promise.reject({ error: 500 });
+
+};
+
+const getWeatherInfo = async (latitude, longitude) => {
+    try {
+        const url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=' + API + '&units=metric';
+        const response = await request(url);
+        console.log(response);
+        document.getElementById("Roma-temp").innerText = response.main.temp;
+        document.getElementById("Roma-icon").src='http://openweathermap.org/img/wn/'+response.weather[0].icon+'@2x.png' ;
+    } catch (err) {
+        console.log(err);
     }
-    getWeather();
+};
+
+const getCityCords = async (name) => {
+    try {
+        const url = 'https://api.openweathermap.org/geo/1.0/direct?q='+name+'&appid='+API;
+        const response = await request(url);
+        console.log(response);
+
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    navigator.geolocation.getCurrentPosition(position => {
+        const { latitude, longitude } = position.coords;
+        getWeatherInfo(latitude, longitude);
+        getCityCords('Roma');
+    })
+});
