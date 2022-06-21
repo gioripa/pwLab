38b7b27cd5dec20060aa815813f906f28706ed6f
gioripa <giorgio.ripamonti@studenti.unimi.it>
2022-06-21 11:43:30 +0200

    aggiunta info citta

--- public/js/weather.js
+++ public/js/weather.js
@@ -11,20 +11,19 @@ const getWeatherInfo = async (latitude, longitude) => {
     try {
         const url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=' + API + '&units=metric';
         const response = await request(url);
-        console.log(response);
-        document.getElementById("Roma-temp").innerText = response.main.temp;
-        document.getElementById("Roma-icon").src='http://openweathermap.org/img/wn/'+response.weather[0].icon+'@2x.png' ;
+        //console.log(response);
     } catch (err) {
         console.log(err);
     }
 };
 
-const getCityCords = async (name) => {
+const getCityInfo = async (name) => {
     try {
-        const url = 'https://api.openweathermap.org/geo/1.0/direct?q='+name+'&appid='+API;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${name},it&appid=${API}&units=metric&lang=it`;
         const response = await request(url);
         console.log(response);
-
+        document.getElementById(`${name}-temp`).innerText = response.main.temp;
+        document.getElementById(`${name}-icon`).src='http://openweathermap.org/img/wn/'+response.weather[0].icon+'@2x.png' ;
     } catch (err) {
         console.log(err);
     }
@@ -35,6 +34,8 @@ document.addEventListener('DOMContentLoaded', () => {
     navigator.geolocation.getCurrentPosition(position => {
         const { latitude, longitude } = position.coords;
         getWeatherInfo(latitude, longitude);
-        getCityCords('Roma');
     })
+    getCityInfo('Roma');
+    getCityInfo('Torino');
+    getCityInfo('Milano');
 });
