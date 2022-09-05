150d45bbfd3f45e69a62562322414399b71e9eb6
gioripa <giorgio.ripamonti@studenti.unimi.it>
2022-09-05 10:29:42 +0200

    fine forecast

--- public/js/weather.js
+++ public/js/weather.js
@@ -34,32 +34,51 @@ document.addEventListener('DOMContentLoaded', async () => {
         var name = x.name;
         document.getElementById("pos").innerText += " "+name;
         document.getElementById("current-icon").src = 'http://openweathermap.org/img/wn/'+x.weather[0].icon+'@2x.png' ;
-        document.getElementById("current-temp").innerText = x.main.temp;
-        document.getElementById("windSpeed").innerText = x.wind.speed;
-        document.getElementById("humidity").innerText = x.main.humidity;
+        document.getElementById("current-temp").innerText = x.main.temp+"°C";
+        document.getElementById("windSpeed").innerText = x.wind.speed+"km/h";
+        document.getElementById("humidity").innerText = x.main.humidity+"%";
         x = await forecast(latitude, longitude);
-        
-        console.log(x);
+        await forecastDisplay(x);
     })
     var response = await getCityInfo('Roma');
-    document.getElementById(`Roma-temp`).innerText = response.main.temp;
+    document.getElementById(`Roma-temp`).innerText = Math.floor(response.main.temp)+"°C";
     document.getElementById(`Roma-icon`).src = 'http://openweathermap.org/img/wn/'+response.weather[0].icon+'@2x.png' ;
     
     response = await getCityInfo('Torino');
-    document.getElementById(`Torino-temp`).innerText = response.main.temp;
+    document.getElementById(`Torino-temp`).innerText = Math.floor(response.main.temp)+"°C";
     document.getElementById(`Torino-icon`).src = 'http://openweathermap.org/img/wn/'+response.weather[0].icon+'@2x.png' ;
     
     response = await getCityInfo('Milano');
-    document.getElementById(`Milano-temp`).innerText = response.main.temp;
+    document.getElementById(`Milano-temp`).innerText = Math.floor(response.main.temp)+"°C";
     document.getElementById(`Milano-icon`).src = 'http://openweathermap.org/img/wn/'+response.weather[0].icon+'@2x.png' ;
 });
 
 const forecast = async (lat,lon) => {
     try {
-        const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API}&units=metric&lang=it`;
+        const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API}&cnt=5&units=metric&lang=it`;
         const response = await request(url);
         return response;
     } catch (err) {
         console.log(err);
+    }
+};
+
+const forecastDisplay = async (data) => {
+    var d = document.getElementById("forecast");
+    console.log(data);
+    var dim = data.cnt;
+    var l = data.list;
+    for(let i=0;i<dim;i++){
+        var f = l[i];
+        var w = f.weather[0];
+        var date = new Date(f.dt_txt);
+        d.innerHTML += `<div class="tempday col">
+        <div>${date.getHours()}:00 </div>
+        <div>${date.toLocaleString('it-it', { weekday: 'long' })}</div>
+        <div>
+            <img src="https://openweathermap.org/img/wn/${w.icon}.png" class = "weather-icon">
+            <p>${Math.floor(f.main.temp)}°C</p>
+        </div>
+      </div>`
     }
 };
