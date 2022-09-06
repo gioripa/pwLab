ce38de545fcb29d8d47357ae8804cc75014aa653
gioripa <giorgio.ripamonti@studenti.unimi.it>
2022-09-06 11:28:04 +0200

    modifiche server

--- server.js
+++ server.js
@@ -1,15 +1,38 @@
+const axios = require("axios").default;
 const express = require("express");
 const fs = require("fs");
 require("dotenv").config();
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname,"/public")));
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
-app.get("/",(req,res) => {
-    res.sendFile(path.join(__dirname,"/views/index.html"));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('.html', require('ejs').renderFile);
+
+const api_key = process.env.API;
+
+app.get("/", (req, res) => {
+    res.render("index.html");
 });
 
-app.listen(process.env.PORTA,() =>{
+app.listen(process.env.PORTA, () => {
     console.log("server avviato");
+});
+
+app.post("/search", (req, res) => {
+    var s = req.body.search;
+    //console.log(s);
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${s},it&appid=${api_key}&units=metric&lang=it`)
+        .then(info => {
+            //console.log(info);
+            return res.render('search',{weather:info.data,error:false});
+        })
+        .catch(error => {
+            console.log(error.message);
+            res.render('search',{weather:null,error:true})
+        });
 });
